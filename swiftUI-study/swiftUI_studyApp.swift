f8301c4305abb826cf8bab4cd6fbe4220f0b2182
kSunaga <jokers4140@gmail.com>
2022-10-07 17:48:13 +0900

    Initial Commit

---
+++ swiftUI-study/swiftUI_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_studyApp.swift
+//  swiftUI-study
+//
+//  Created by sunaga on 2022/10/07.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
